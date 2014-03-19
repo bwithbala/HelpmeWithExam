7081b1533800cf00168da2bb042a1e6c39c5e7ea
bwithbala <balakrishnan.tharumar@gmail.com>
2014-03-19 16:39:29 -0400

    Commit

--- HelpmeWithExam/assets/www/view/RWFractions.view.js
+++ HelpmeWithExam/assets/www/view/RWFractions.view.js
@@ -29,7 +29,7 @@ sap.ui.jsview("view.RWFractions", {
 			layoutData: new sap.m.FlexItemData({growFactor: 1})
 		});*/
 		
-		var oLabelFraction = new sap.m.Label({
+/*		var oLabelFraction = new sap.m.Label({
 			text: "Choose 5 fractions which matches the given Fraction. ",
 			design: "Bold",
 			layoutData: new sap.m.FlexItemData({growFactor: 1})
@@ -45,7 +45,7 @@ sap.ui.jsview("view.RWFractions", {
 			text: "Choose from Below",
 			design: "Bold",
 			layoutData: new sap.m.FlexItemData({growFactor: 3})
-		});		
+		});		*/
 		
 		
 /*       var oCheckboxTemplate = new sap.m.Checkbox({
@@ -60,11 +60,11 @@ sap.ui.jsview("view.RWFractions", {
        
        
        
-		var cbOption1 = new sap.m.CheckBox({
+/*		var cbOption1 = new sap.m.CheckBox({
 			selected:"{/selected}", visible: "{/visible}", text: "{/text}",  enabled: "{/enabled}"}
 		    layoutData: new sap.m.FlexItemData({growFactor: 2})
 	});
-		);
+		);*/
 	/*	var cbOption2 = new sap.m.CheckBox({selected:true, visible: true, text: "2/4 * 2/2 = 2/8", enabled: true});
 		var cbOption3 = new sap.m.CheckBox({selected:true, visible: true, text: "3/4 * 3/3 = 2/8", enabled: true});
 		
@@ -74,21 +74,50 @@ sap.ui.jsview("view.RWFractions", {
 		
 		
 	
-		// Create a vertical flexbox with items
+/*		// Create a vertical flexbox with items
 		var oVBox1 = new sap.m.VBox("vbox1", {
 			items:[
 				oLabelFraction,
 				cbOption1
 				
 			]
-		});    	
+		});    	*/
     	
 		       
 
+		var oFlexBox = new sap.m.FlexBox();
+		
+		oFlexBox.addItem(
+				new sap.m.Label({
+					text: "Choose 5 fractions which matches the given Fraction. ",
+					design: "Bold",
+					layoutData: new sap.m.FlexItemData({growFactor: 1})
+				})				
+				
+		);
+		
+		oFlexBox.addItem(
+				
+				 new sap.m.Label({
+					text: "1/4",
+					design: "Bold",
+					layoutData: new sap.m.FlexItemData({growFactor: 1})
+				})		
+				
+				);
+		
+		oFlexBox.addItem(
+				
+				 new sap.m.Label({
+     				text: "Choose from Below",
+					design: "Bold",
+					layoutData: new sap.m.FlexItemData({growFactor: 1})
+				})		
+				);    	
 
         return new sap.m.Page({
             title: "Read Write Fractions",
-            content: [oVBox1],
+            content: [oFlexBox],
             headerContent: [],
             footer: new sap.m.Bar({})
         });
